0742fe65539cc491b126339a1d3924ba97070847
Crowdbotics <team@crowdbotics.com>
2020-11-18 16:12:20 +0000

    add package blueprint-react-native-message-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Messaging1175341', human_name: 'Messaging 1', access_route: 'Messaging1175341', icon: 'comments'},
 { name: 'SignIn22175340', human_name: 'Sign In 2 2', access_route: 'SignIn22175340'},
 { name: 'Settings3175339', human_name: 'Settings 3', access_route: 'Settings3175339'},
 
